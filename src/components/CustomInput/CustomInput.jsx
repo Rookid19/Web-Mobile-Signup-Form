38430d31cbf5c8603a848d7883f89d593e29f97d
Randy Odoom <randyodoom19@gmail.com>
2023-01-10 20:58:11 +0000

    countries dropdown

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function CustomInput() {
+function CustomInput({ placeholder }) {
   return (
     <div>
-        <p>We send a verification</p>
-        <input className='custom-input' placeholder=''/>
+      <p>We send a verification</p>
+      <input className="custom-input" placeholder={placeholder} style={{
+        width:"70%"
+      }}/>
     </div>
-  )
+  );
 }
 
-export default CustomInput
+export default CustomInput;
--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -4,6 +4,10 @@ import "./Phone.css";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 function Phone() {
+  const countries = [
+    { id: 1, country: "US", flag: "fi fi-us", label: "🇺🇸 United States (+1)" },
+    { id: 2, country: "GH", flag: "fi fi-gh", label: "🇬🇭 GHA (+233)" },
+  ];
   return (
     <div>
       <span className="step">Step 1 of 6</span>
@@ -11,10 +15,17 @@ function Phone() {
       <div className="sub-title">
         Select your country and enter your phone number
       </div>
-      <div className="phone-container">
+      {/* <div className="phone-container">
         <span class="fi fi-gh"></span> <span class="fi fi-us"></span>
-      </div>
-      <CustomInput />
+      </div> */}
+      {/* <CustomInput placeholder="Code" /> */}
+      <select>
+        {countries.map(({ id, label, flag }) => (
+          <option value="fruit" key={id}>
+            {label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
